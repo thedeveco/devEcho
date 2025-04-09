c0ca8e4d67520e64c8927ca7d17e9a0792e609a4
Sdriver1 <sjdriver08@gmail.com>
2025-04-09 09:02:22 -0400

    Adding DB

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 import importlib
+from database.profile import profiledb
 
 load_dotenv()
 TOKEN = os.getenv("BotToken")
@@ -16,15 +17,23 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-    for filename in os.listdir("./src/commands"):
-        if filename.endswith(".py"):
-            module = f"src.commands.{filename[:-3]}"
-            importlib.import_module(module).setup(bot)
-    
+    for root, _, files in os.walk("./commands"):
+        for filename in files:
+            if filename.endswith(".py"):
+                path = os.path.join(root, filename)
+                rel_path = os.path.relpath(path, ".").replace("\\", "/")
+                module = rel_path[:-3].replace("/", ".")
+                try:
+                    importlib.import_module(module).setup(bot)
+                    print(f"Loaded {module}")
+                except Exception as e:
+                    print(f"Failed to load {module}: {e}")
+
     try:
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} command(s).")
     except Exception as e:
         print(f"Error syncing commands: {e}")
 
+profiledb.init_db()
 bot.run(TOKEN)
